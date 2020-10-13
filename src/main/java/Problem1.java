79ed18e8aee8bb87dbe16dea92f575b77ba88467
SirAJn8r <hyperactive@live.com>
2020-10-12 17:52:27 -0700

    Fixed an indenting error within a comment

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,7 +4,7 @@ public class Problem1
     Describe your algorithm here in up to 5 sentences:
     It loops through the entire loop once, when encountering a 2 it does nothing but increment i, which is used to deduce the number of 2's without a running counter.
     When encountering a 1, it changes the theoretical beginning of the 2 section to a 1, if there is no 2's it will change itself (the current i).
-        If there is a 2, then it will change the current value (at i) to be a 2, so that the number of 2's is preserved.
+    If there is a 2, then it will change the current value (at i) to be a 2, so that the number of 2's is preserved.
     When encountering a 0, it will basically do what 1 did in relation to 2's but twice, one for each.
     */
 
